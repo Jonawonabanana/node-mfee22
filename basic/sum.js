2063a8144181de7bf1d4cd82ef640ea59723d749
Jona Lin <jona25583@gmail.com>
2021-12-26 14:27:57 +0800

    -add sum.js

---
+++ basic/sum.js
@@ -0,0 +1,12 @@
+function sum(n){
+    let result=0;
+    for(let i=1; i<n+1; i++)
+        result+=i;
+
+    return result;
+}
+
+console.log(sum(1));
+console.log(sum(2));
+console.log(sum(5));
+console.log(sum(10));
